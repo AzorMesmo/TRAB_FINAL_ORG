04e097857ca478a95c4aba71b8b5b3b526fd685a
Azor <mateusazorfrutuoso@gmail.com>
2022-12-19 08:45:06 -0300

    Updated Default File

--- BatalhaNaval.asm
+++ BatalhaNaval.asm
@@ -1,65 +1,44 @@
-		.data
+# Mateus Azor - 2011100013
+# Nat�lia Banhara - XXXXXXXXXX
 
-navios:		.asciz	"3\n1 5 1 1\n0 5 2 2\n0 1 6 4"
+	.data
 
-matriz_navios: 	  .space 	400
+ship_list:	.asciz	"3\n1 5 1 1\n0 5 2 2\n0 1 6 4"
 
+ship_matrix: 	.space 	400
 
 	.text
 main: 
 
-	la 	a0, navios  			# imprime string com posição dos navios
-	li 	a7,4
-	ecall
-
-
+	la 	a0, ship_list			# carrega o endereco de ship_list em a0
+	li 	a7,4				# carrega o imediato 4 em a7 (registrador de system calls)
+	ecall					# faz a chamada de sistema
 	
-	la 	a0, navios  			# a0 recebe o endereço inicial da string navios
-	la	a1, matriz_navios 		# a1 recebe o endereço inicial da matriz navios
-	jal	insere_embarcacoes		# chama função que insere os navios
+	la 	a0, ship_list  			# carrega o endereco de ship_list em a0
+	la	a1, ship_matrix 		# carrega o endereco de ship_matrix em a0
+	jal	insert_ships			# chama a funcao insert_ships (ra -> t1)
 
-
-
-	la 	a0, matriz_navios  		# a0 recebe o endereço inicial da string navios
+	la 	a0, ship_matrix  		# a0 recebe o endereço inicial da string navios
 	li	a1, 1				# a1 indica que deve mostrar os navios
-	jal	imprime_interface_navios	# imprime matriz depois de posicionar os navios
+	jal	print_ships			# imprime matriz depois de posicionar os navios
 
+main_end:	
 
-
-
-fim:	
 	nop
 	li   a7, 10
 	ecall
 
-#########################################################
-#insere_embarcacoes
-# argumentos: a0 - endereço inicial da matriz navios
-#             a1 - 0 imprime interface - 1 imprime navios
-# retorno: ---- (nenhum retorno)
-# comentário: 
-#
-#########################################################
-insere_embarcacoes:
+# Nome: insert_ship
+# Argumentos:
+# Retorno:
 
-		
+insert_ships:
 
-	ret	
+	ret
 
+# Nome: print_ships
+# Argumentos:
+# Retorno:
+print_ships:
 
-
-
-#########################################################
-# imprime_matriz_navios
-# argumentos: a0 - endereço inicial da matriz navios
-#             a1 - 0 imprime interface - 1 imprime navios
-# retorno: ---- (nenhum retorno)
-# comentário: 
-#
-#########################################################
-imprime_interface_navios:
-
-
-
-	ret	
-
+	ret
