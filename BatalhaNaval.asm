47a577b4d62b4cb183449e4d9dd7ddc123e61aa9
Azor <mateusazorfrutuoso@gmail.com>
2022-12-23 09:48:49 -0300

    Update Insertion

--- BatalhaNaval.asm
+++ BatalhaNaval.asm
@@ -12,11 +12,15 @@ ship_matrix: 	.space 	400			# 10 x 10 x 4 (integer size)
 
 new_line:	.string "\n"
 
+space:		.string " "
+
 	.text
-main: 
-	# tirei do a0 pq agnt sempre usa o a0 pras system calls
+
+# TUDO QUE NAO ESTA COMENTADO NAO ESTARA PRESENTE NO CODIGO FINAL (TIPO PRINT PRA VER SE TA FUNCIONANDO E COISAS ASSIM)
+
+main:
 	
-	la a1, ship_list			# carrega o endereco de ship_list em a0
+	la a1, ship_list			# carrega o endereco de ship_list em a1
 	mv a0, a1				# carrega o valor de a1 em a0
 	li a7,4					# carrega o imediato 4 (print string) em a7 (registrador de system calls)
 	ecall					# faz a chamada de sistema
@@ -40,37 +44,99 @@ main_end:
 
 insert_ships:
 	
-	lb t1, 0(a1)				# carrega o primeiro byte de a0 em t1 (contador de barcos)
+	lb t1, 0(a1)				# carrega o primeiro byte de a1 em t1 (contador de barcos)
+	addi t1, t1, -48			# subtrai 48 do valor de t1 (convertendo o numero em string para inteiro)
+	addi t2, zero, 4			# carrega o valor 4 em t2 (numero de instrucoes por barco)
 	
 	la a0, new_line
 	li a7, 4
 	ecall
+	ecall
 	
-	addi t1, t1, -48			# subritai 48 do valor de t1 (convertendo o numero em string para inteiro)
+is_get_values:
+
+	# t1 -> contador de barcos
+	# t2 -> direcao do barco
+	# t3 -> tamanho do barco
+	# t4 -> linha inicial do barco
+	# t5 -> coluna inicial do barco
+
+	beq t1, zero, end_insert_ships		# desvia se t1 (contador de barcos) for igual a 0
+	
+	addi a1, a1, 2				# aponta o endereço de a1 para o próximo valor relevante
+	lb t2, 0(a1)				# carrega o proximo byte de a1 em t2 (direcao do barco)
+	addi t2, t2, -48			# subtrai 48 do valor de t2 (convertendo o numero em string para inteiro)
 	
 	mv a0, t2
 	li a7, 1
 	ecall
 	
-	#acho que agnt vai ter que ter dois loops, ou ler e inserir no mesmo loop (gosto mais dessa ideia se pa)
+	la a0, space
+	li a7, 4
+	ecall
 	
-	# melhor pegar esses valores direto no loop ? pode ser mais facil ? n sei..
-	#pegar o valor da horizontal ou vertical em t2
-	#pegar o comprimento do navio em t3
-	#depois comeca o looping
+	addi a1, a1, 2				# aponta o endereço de a1 para o próximo valor relevante
+	lb t3, 0(a1)				# carrega o proximo byte de a1 em t3 (tamanho do barco)
+	addi t3, t3, -48			# subtrai 48 do valor de t3 (convertendo o numero em string para inteiro)
 	
-loop_insert_ships:
-
-	beq t1, zero, end_insert_ships		# desvia se t1 (contador de barcos) for igual a 0
-		#looping de cada navio -> inserÃ§Ã£o
-		#antes de inserir o navio, checar exceÃ§Ãµes
-		
-		
-		
+	mv a0, t3
+	li a7, 1
+	ecall
 	
+	la a0, space
+	li a7, 4
+	ecall
+	
+	addi a1, a1, 2				# aponta o endereço de a1 para o próximo valor relevante
+	lb t4, 0(a1)				# carrega o proximo byte de a1 em t4 (linha inicial do barco)
+	addi t4, t4, -48			# subtrai 48 do valor de t4 (convertendo o numero em string para inteiro)
+	
+	mv a0, t4
+	li a7, 1
+	ecall
+	
+	la a0, space
+	li a7, 4
+	ecall
+	
+	addi a1, a1, 2				# aponta o endereço de a1 para o próximo valor relevante
+	lb t5, 0(a1)				# carrega o proximo byte de a1 em t5 (coluna inicial do barco)
+	addi t5, t5, -48			# subtrai 48 do valor de t5 (convertendo o numero em string para inteiro)
+	
+	mv a0, t5
+	li a7, 1
+	ecall
+	
+	la a0, space
+	li a7, 4
+	ecall
 
+is_check_values:
 
-end_insert_ships:
+	# verifica se as posicoes sao validas
+	
+	# testes (caso a do pdf):
+		# direcao >= 0 e <= 1
+		# tamanho, linha e coluna iniciais >= 0 e <= 9
+		
+	# testes (caso b do pdf):
+		# se direcao = 0, tamanho + linha inicial <= 10
+		# se direcao = 0, tamanho + coluna inicial <= 10
+		
+	# testes (caso c do pdf):
+		# deve existir uma formula matematica simples que detecta a colisao entre dois vetores em uma matriz (provavelmente coisa de ga) mas eu n conheco então.....
+		# na hora de colocar um navio, checar se cada uma de suas coordenadas nao esta ocupada por outro navio
+
+is_set_values:
+
+	# coloca os valores na matriz
+	
+	addi t1, t1, -1				# decrementa 1 de t1 (contador de barcos)
+	
+	j loop_insert_ships			# desvia para loop_insert_ships
+
+is_end:
+
 	ret
 
 # Nome: print_ships
@@ -86,6 +152,7 @@ print_ships:
 # Retorno: nenhum
 
 reset:
+
 	addi a1, s0, 0				# carrega s0 em a1 (volta a ship_list para a posicao inicial)
 	addi a2, s1, 0				# carrega s1 em a2 (volta a ship_matrix para a posicao inicial)
 	ret					# retorna
