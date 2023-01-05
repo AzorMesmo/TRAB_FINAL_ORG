e204f1734de53b9ca1d517822a51d1c5dc7ed824
Natália Banhara <natalia.banhara@outlook.com>
2023-01-05 09:23:12 -0300

    Update BatalhaNaval.asm

--- BatalhaNaval.asm
+++ BatalhaNaval.asm
@@ -6,7 +6,7 @@
 
 	.data
 
-navios:	.asciz	"3\n1 5 1 1\n0 5 2 2\n0 1 6 4"
+navios:	.asciz	"3\n1 9 1 1\n0 8 2 2\n0 1 6 4"
 
 matriz_navios: 	.space 	400			# 10 x 10 x 4 (integer size)
 
@@ -23,15 +23,15 @@ here:		.string "Here\n"
 main:
 	
 	la a0, navios				# carrega o endereco de navios em a0
-	la a1, matriz_navios			# carrega o endereço de ship_matrix em a1
+	la a1, matriz_navios			# carrega o endereço de matriz_navios em a1
 	
 	addi s4, zero, 0			# s4 possui o número que cada navio vai ter na matriz
 	
-	call insere_embarcacoes			# chama a função insere_embarcacoes
+	jal insere_embarcacoes			# chama a função insere_embarcacoes
 	
-	call reset
+	jal reset				# chama a função reset
 	
-	call imprime_interface_navios
+	jal imprime_interface_navios		# chama a função imprime_interface_navios
 
 main_end:	
 
@@ -43,144 +43,118 @@ main_end:
 #insere_embarcacoes
 # argumentos: a0 - endereço inicial da string navios
 #             a1 - endereço inicial da matriz de navios (matriz_navios)
-# retorno: a string navios agora possui os navios lidos 
-#          e a matrix possui os navios inseridos nela, caso sejam válidos
-# comentário: a função primeiramente lê a quantidade de navios a serem inseridos, 
-#             depois os navios, checando caso sejam válidos e os inserindo na matrix
+# retorno: a1 - matriz preenchida com os navios
+# comentário: a função lê os navios a serem inseridos, checando caso sejam válidos e os inserindo na matrix
 #
 #########################################################
 insere_embarcacoes:
-	j le_navios
-
-le_navios: 
-	addi a1, zero, 2     # a1 possui o tamanho máximo de caracteres que será lido
+	lb t1, 0(a0)			# t1 -> contador de barcos (em ascii)
+	addi t1, t1, -48			# subtrai 48 do valor de t1 (convertendo o numero em string para inteiro)
 	
-	li a7, 8
-	ecall
-	
-	lb t1, 0(a0)
-	addi t1, t1, -48
-	
-	addi a0, a0, 2
-	
-	li a7, 8
-	ecall
-	
-loop_input:
-	beq t1, zero, end_input
-	addi t2, zero, 8 			# t2 recebe 8 para ler um navio	
-	addi s0, a0, 0
-loop_input_one:
-	beq t2, zero, insere_um_navio
-	
-	li a7, 8
-	ecall
-	
-	addi a0, a0, 2
-	
-	addi t2, t2, -1
-	j loop_input_one
-	
-
-end_loop_input_one:
-	addi t1, t1, -1
-	addi a1, zero, 2     # a1 possui o tamanho máximo de caracteres que será lido
-	
-	j loop_input
-	
-end_input:
-	ret
+loop_insere_embarcacoes:
+	beq t1, zero, fim_insere_embarcacoes                 # desvia se já leu todos os navios
 	
 insere_um_navio:
-	
-	la a1, matriz_navios
-	
+
 	# t1 -> contador de barcos
 	# t2 -> direcao do barco
 	# t3 -> tamanho do barco
 	# t4 -> linha inicial do barco
 	# t5 -> coluna inicial do barco
 	
-	addi a0, s0, 0				# a0 -> posição inicial do barco na string navios
+	addi a0, a0, 2				# a0 -> posição inicial do barco na string navios
 	lb t2, 0(a0)				# carrega o proximo byte de a0 em t2 (direcao do barco)
-	addi t2, t2, -48			# subtrai 48 do valor de t2 (convertendo o numero em string para inteiro)
 	
-	addi a0, a0, 4				# aponta o endere�o de a0 para o pr�ximo valor relevante
+	addi a0, a0, 2				# aponta o endere�o de a0 para o pr�ximo valor relevante
 	lb t3, 0(a0)				# carrega o proximo byte de a0 em t3 (tamanho do barco)
-	addi t3, t3, -48			# subtrai 48 do valor de t3 (convertendo o numero em string para inteiro)
 	
-	addi a0, a0, 4				# aponta o endere�o de a0 para o pr�ximo valor relevante
+	addi a0, a0, 2				# aponta o endere�o de a0 para o pr�ximo valor relevante
 	lb t4, 0(a0)				# carrega o proximo byte de a0 em t4 (linha inicial do barco)
-	addi t4, t4, -48			# subtrai 48 do valor de t4 (convertendo o numero em string para inteiro)
 	
-	addi a0, a0, 4				# aponta o endere�o de a0 para o pr�ximo valor relevante
+	addi a0, a0, 2				# aponta o endere�o de a0 para o pr�ximo valor relevante
 	lb t5, 0(a0)				# carrega o proximo byte de a0 em t5 (coluna inicial do barco)
+	
+	addi t2, t2, -48			# subtrai 48 do valor de t2 (convertendo o numero em string para inteiro)
+	addi t3, t3, -48			# subtrai 48 do valor de t3 (convertendo o numero em string para inteiro)
+	addi t4, t4, -48			# subtrai 48 do valor de t4 (convertendo o numero em string para inteiro)
 	addi t5, t5, -48			# subtrai 48 do valor de t5 (convertendo o numero em string para inteiro)
 	
-	addi s10, a0, 0
 	
-	addi a0, t2, 0
-	li a7, 1
+	jal s9, checa_valores			# chama função de checagem da validade dos navios e guarda em s9 o endereço de retorno
+	
+	beq a2, zero, fim_insere_embarcacoes_erro  # desvia para fim_insere_embarcacoes_erro caso um erro seja detectado no navio
+	
+	la a1, matriz_navios			# a1 -> endereço inicial da matriz
+	
+	jal s9, coloca_valores			# chama função coloca_valores e guarda em s9 o endereço de retorno
+	
+	addi t1, t1, -1				# decrementa t1 em 1
+	
+	j loop_insere_embarcacoes
+	
+fim_insere_embarcacoes:
+	ret
+	
+fim_insere_embarcacoes_erro:		# imprime mensagem de erro caso um dos navios não seja válido e retorna
+	la a0, error_message
+	li a7, 4
 	ecall
+	ret
 	
-	addi a0, t3, 0
-	li a7, 1
-	ecall
-	
-	addi a0, t4, 0
-	li a7, 1
-	ecall
-	
-	addi a0, t5, 0
-	li a7, 1
-	ecall
-	
-	addi a0, s10, 0
 
 
-#começo da função de checagem #ARRUMAR PARA SEREM CHAMADAS DE FUNÇÕES SIMPLES
-checks_values:
+#########################################################
+# checa_valores
+# argumentos: a1 -> matriz_navios
+# retorno: a2 = 1 (se for válida)
+#	   a2 = 0 (se não for válida)
+# comentário: checa se os valores do barco são válidos
+#
+#########################################################
+checa_valores:
 	addi s2, zero, 9			# S2 -> 9 (número máximo que uma linha ou coluna pode ter)
-	
-checks_position:
-	bgt t4, s2, error_found			# identifica erro se a linha for maior que 9
-	bgt t5, s2, error_found			# identifica erro se a coluna for maior que 9
+checa_posicao:
+	bgt t4, s2, erro_encontrado		# identifica erro se a linha for maior que 9
+	bgt t5, s2, erro_encontrado		# identifica erro se a coluna for maior que 9
 
-	blt t4, zero, error_found		# identifica erro se a linha for menor que 0
-	blt t5, zero, error_found		# identifica erro se a coluna for menor que 0
+	blt t4, zero, erro_encontrado		# identifica erro se a linha for menor que 0
+	blt t5, zero, erro_encontrado		# identifica erro se a coluna for menor que 0
 	
-checks_first_digit:				# checa o primeiro digito -> deve ser igual a 0 ou 1
+checa_primeiro_digito:				# checa o primeiro digito -> deve ser igual a 0 ou 1
 	addi t6, zero, 1			# t6 -> 1 (opção vertical)
-	bgt t2, t6, error_found			# identifica erro se a posição for maior que 1
-	blt t2, zero, error_found		# identifica erro se a posição for menor que 0
+	bgt t2, t6, erro_encontrado			# identifica erro se a posição for maior que 1
+	blt t2, zero, erro_encontrado		# identifica erro se a posição for menor que 0
 	
-checks_length:
+checa_tamanho:
 	addi t6, zero, 10			# t6 -> 10 (quantidade de linhas e colunas)
-	bgt t3, t6, error_found			# identifica erro se o tamanho do barco for maior que 10
-	blt t3, zero, error_found		# identifica erro se o tamanho do barco for menor que 0
+	bgt t3, t6, erro_encontrado			# identifica erro se o tamanho do barco for maior que 10
+	blt t3, zero, erro_encontrado		# identifica erro se o tamanho do barco for menor que 0
 	
-checks_horizontal:
-	bne t2, zero, checks_vertical		# desvia para checks_vertical se o barco for vertical
+checa_horizontal:
+	bne t2, zero, checa_vertical		# desvia para checks_vertical se o barco for vertical
 	add t6, t5, t3				# t6 -> tamanho do barco + coluna inicial (para verificar se ultrapassa as dimensoes)
 	
-checks_vertical:
-	add t6, t4, t3				# t6 -> tamanho do barco + linha inicial (para verificar se ultrapassa as dimensoes)
+	j checa_limite
 	
-checks_overdimension:
+checa_vertical:
+	add t6, t4, t3				# t6 -> tamanho do barco + linha inicial (para verificar se ultrapassa as dimensoes)
+
+checa_limite:
+	addi t6, t6, -1
+	bgt t6, s2, erro_encontrado
+
+checa_sobreposicao:
 
 	# t3 -> tamanho do barco -> s6
 	# t4 -> linha inicial do barco -> s8 se vertical
 	# t5 -> coluna inicial do barco -> s7 se horizontal
 	
-	bgt t6, s2, error_found
-	
 	addi s6, t3, 0 				# s6 -> tamanho do barco
-	
 	addi s7, t5, 0				# s7 -> coluna inicial
 	addi s8, t4, 0				# s8 -> linha inicial
 	
-loop_checks_overdimension:
-	bge zero, s6, checks_values_ends	# desvia para checks_values_ends se já passou por todas posições que o barco ocupará
+loop_checa_sobreposicao:
+	bge zero, s6, fim_checa_valores	# desvia para checks_values_ends se já passou por todas posições que o barco ocupará
 	
 	la a1, matriz_navios			# a1 -> posição inicial da matriz
 	
@@ -194,49 +168,40 @@ loop_checks_overdimension:
 
 	lb s5, 0(a1)				# s5 -> valor na posição a1
 	
-	bne s5, zero, error_found		# identifica erro se o valor lido for diferente de 0
+	bne s5, zero, erro_encontrado		# identifica erro se o valor lido for diferente de 0
 	
-	bne t2, zero, checks_overdimension_vertical	# desvia para checks_overdimension_vertical caso o barco for vertical
+	bne t2, zero, checa_sobreposicao_vertical	# desvia para checks_overdimension_vertical caso o barco for vertical
 	
-checks_overdimension_horizontal:
+checa_sobreposicao_horizontal:
 	addi s7, s7, 1				# incrementa em 1 o número de colunas
-	j overdimension_set_decrement
+	j checa_sobreposicao_decrementa
 	
-checks_overdimension_vertical:
+checa_sobreposicao_vertical:
 	addi s8, s8, 1				# incrementa em 1 o número de linhas
 	
-overdimension_set_decrement:
+checa_sobreposicao_decrementa:
 	addi s6, s6, -1				# diminui em 1 o tamanho do barco para continuar a percorrer por ele
-	j loop_checks_overdimension	
+	j loop_checa_sobreposicao	
 	
-checks_values_ends:
-	j set_values	
+fim_checa_valores:
+	addi a2, zero, 1			# a2 = 1 (válida)
+	jr s9
 
-error_found:
-	la a0, error_message			# mostra mensagem de erro
-	li a7, 4
-	ecall
-	
-	ret					# retorna
-	# verifica se as posicoes sao validas
-	
-	# testes (caso a do pdf):
-		# direcao >= 0 e <= 1
-		# tamanho, linha e coluna iniciais >= 0 e <= 9
-		
-	# testes (caso b do pdf):
-		# se direcao = 0, tamanho + linha inicial <= 10
-		# se direcao = 0, tamanho + coluna inicial <= 10
-		
-	# testes (caso c do pdf):
-		# deve existir uma formula matematica simples que detecta a colisao entre dois vetores em uma matriz (provavelmente coisa de ga) mas eu n conheco ent�o.....
-		# na hora de colocar um navio, checar se cada uma de suas coordenadas nao esta ocupada por outro navio
+erro_encontrado:
+	addi a2, zero, 0			# a2 = 0 (inválida)
+	jr s9					# retorna
 
-set_values: 			# inserts one boat
-	la a1, matriz_navios		# a1 -> posição inicial da matrix
+#########################################################
+# coloca_valores
+# argumentos: a1 -> matriz_navios
+# retorno: a1 possui o barco inserido nela
+# comentário: insere um barco na matriz
+#
+#########################################################
+coloca_valores: 			
 	addi s4, s4, 1 			# incrementa o número que representa o navio na matrix
-loop_set_values:
-	bge zero, t3, set_end		# percorre todas as posições do navio até tamanho ser menor ou igual a 0
+loop_coloca_valores:
+	bge zero, t3, fim_coloca_valores		# percorre todas as posições do navio até tamanho ser menor ou igual a 0
 	
 	la a1, matriz_navios
 	
@@ -250,33 +215,30 @@ loop_set_values:
 	
 	sb s4, 0(a1)			# a posição de a1 recebe o valor que identifica o barco
 	
-	bne t2, zero, set_vertical	# desvia para set_vertical se o barco for vertical
-set_horizontal:
+	bne t2, zero, coloca_valores_vertical	# desvia para coloca_valores_vertical se o barco for vertical
+coloca_valores_horizontal:
 	addi t5, t5, 1			# t5 + 1 -> aumenta coluna inicial
-	j set_decrement			
+	j coloca_valores_decrementa			
 	
-set_vertical:
+coloca_valores_vertical:
 	addi t4, t4, 1			# t4 + 1 -> aumenta linha inicial
 	
-set_decrement:
+coloca_valores_decrementa:
 	addi t3, t3, -1			# decrementa o tamanho do barco em 1 para continuar preenchendo na matriz
-	j loop_set_values
+	j loop_coloca_valores
 	
-set_end:
-	j end_loop_input_one			# desvia para loop_insert_ships
-
-#ARRUMAR:::::::::::::::::::::::::::::::::::::::::::::::::::;IMPORTANT (CABEÇALHO)
+fim_coloca_valores:
+	jr s9
 
 #########################################################
 # imprime_matriz_navios
-# argumentos: a0 - endereço inicial da matriz navios
-#             a1 - 0 imprime interface - 1 imprime navios
-# retorno: ---- (nenhum retorno)
-# comentário: 
+# argumentos: a1 -> matriz_navios
+#	      a0 -> navios
+# retorno:(nenhum retorno)
+# comentário: imprime a matriz
 #
 #########################################################
 imprime_interface_navios:
-	la a1, matriz_navios
 	
 	addi a4, a1, 0 # coloca o valor de a1 (endereço incial da matriz) em a4
 	addi t0, zero, 100 # contador do tamanho da matriz (t0 recebe o numero de elementos da matriz)
@@ -314,9 +276,12 @@ p_end:
 
 	ret # retorna
 
-#ARRUMAR:::::::::::::::::::::::::::::::::::::::::::::::::::;IMPORTANT (CABEÇALHO)
+#########################################################
+# imprime_matriz_navios
+# retorno: coloca em a0 a posição inicial da string navios e em a1 a posição inicial da matriz_navios
+#########################################################
 reset:
 
-	la a0, navios				# carrega s0 em a1 (volta a ship_list para a posicao inicial)
-	la a1, matriz_navios				# carrega s1 em a2 (volta a ship_matrix para a posicao inicial)
+	la a0, navios				# carrega a string navios em a0
+	la a1, matriz_navios			# carrega a matriz_navios em a1
 	ret					# retorna
